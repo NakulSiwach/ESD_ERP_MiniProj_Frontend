ff49e20897f2a9fc1a118cb0660b12dd54abf182
NakulSiwach <nakulsiwach007@gmail.com>
2024-11-23 22:12:18 +0530

    employee details api fetch and display

---
+++ src/Utils/httputils.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const API_BASE_URL = "http://localhost:8080/api/v1";
+
+// Generic Axios instance
+const axiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json"  
+  },
+});
+
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("jwt");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export const fetchEmployeesAPI = async () => {
+  const response = await axiosInstance.get("/employees");
+  return response.data;
+};
+
+export const updateEmployee = async (employee) => {
+  const response = await axiosInstance.put("/employees", employee);
+  return response.data;
+};
+
+export const loginUser = async (email, password) => {
+  const response = await axiosInstance.post("/employees/login", { email, password });
+  localStorage.setItem("jwt",response.data.token)
+  console.log(response.data)
+  return response.data;
+};
+
+export const disburseSalaries = async (ids) => {
+  console.log(ids);
+  const response  = await axiosInstance.post("/employees/disburse",ids);
+  return response;
+};
